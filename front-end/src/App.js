c2a8b329837841980f8b03114d6d4ea12a120cfa
zacfrappier <zacfrappier@gmail.com>
2023-04-13 17:52:05 -0700

    auth0 components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import TopNav from "./components/topNav/TopNav";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
   return (
-    <Auth0Provider>
     <div>
 
       <TopNav />
     </div>
-    </Auth0Provider>
   );
 }
 
