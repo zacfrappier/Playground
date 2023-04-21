a19b9a891c784d0d764c721530d36f0014c1aa91
zacfrappier <zacfrappier@gmail.com>
2023-04-20 23:38:53 -0700

    setup up back and front end

--- front-end/src/service/service.js
+++ front-end/src/service/service.js
@@ -1,26 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-const mongoURI = process.env.MONGO_URI;
-const API_URL = 'mongoURI';
+const mongoURI = process.env.REACT_APP_MONGO_URI;
 
 const getAllPrompts = () => {
-  return axios.get(`${API_URL}/items`);
+  return axios.get(`${mongoURI}/tasks`);
 };
 
+//    ----------With success and error handlers---------------------
+// const getAllPrompts = () => {
+//     const config= {
+//         method: "GET",
+//         url: `${mongoURI}/tasks`,
+//         crossdomain: true,
+//         headers: {"Content-type": "application/json"}
+//     };
+//     return axios(config); // .then(onSuccess).catch(onError);
+//   };
+
 const getPromptById = (id) => {
-  return axios.get(`${API_URL}/items/${id}`);
+  return axios.get(`${mongoURI}/tasks/${id}`);
 };
 
 const createPrompt = (data) => {
-  return axios.post(`${API_URL}/items`, data);
+  return axios.post(`${mongoURI}/tasks`, data);
 };
 
 const updatePrompt = (id, data) => {
-  return axios.put(`${API_URL}/items/${id}`, data);
+  return axios.put(`${mongoURI}/tasks/${id}`, data);
 };
 
 const deletePrompt = (id) => {
-  return axios.delete(`${API_URL}/items/${id}`);
+  return axios.delete(`${mongoURI}/tasks/${id}`);
 };
 
 export default {
