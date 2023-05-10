4c3acfe23747f9e9755cc1c41b1187885824ddfb
Tyler Tulsky <ttulsky@gmail.com>
2023-05-10 15:16:30 +0900

    dummy api added for port 5000

---
+++ back-end/dummy_api/api.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+// const Jwt = require("express-jwt");
+// const Jwks = require("jwks-rsa");
+const { auth } = require("express-oauth2-jwt-bearer");
+
+const jwtCheck = auth({
+  audience: "http://localhost:5000",
+  issuerBaseURL: "https://dev-ezr2jpqdbryydw6b.us.auth0.com/",
+  tokenSigningAlg: "RS256",
+});
+// this requires all requests to have a Jwt check
+// app.use(jwtCheck);
+
+app.get("/public", (req, res) => {
+  res.json({
+    type: "public",
+  });
+});
+// by passing the jwtcheck into the request we isolate the requierment to specific requests vs requiering all requests to have the jwtcheck
+app.get("/private", jwtCheck, (req, res) => {
+  res.json({
+    type: "private",
+  });
+});
+
+app.listen(5000);
